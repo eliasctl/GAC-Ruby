e259893ad0273dd231ee4c9cd77544380791a6c6
Le_Vrai_WIWI <superwoup@gmail.com>
2022-12-05 22:16:24 +0100

    first function

--- Bases/helloword.rb
+++ Bases/helloword.rb
@@ -1,1 +1,21 @@
-puts "Hello World"
+def newBoard(n)
+    tab = []
+    for i in 0..n
+        tab.push([])
+        for j in 0..n
+            tab[j].push(0)
+        end
+    end
+    return tab
+end
+
+
+n = 14
+for i in 0..n
+    puts i
+end
+
+puts "Enter la taille du tableau "
+taille = gets.chomp.to_i
+puts taille
+newBoard(taille)
